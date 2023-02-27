8442c0acc6fa440bbde33dd18f0c0e8fdcd6b49d
c-jaenicke <me@chrisj.de>
2023-02-27 14:22:52 +0100

    Comment

--- pkg/item/item.go
+++ pkg/item/item.go
@@ -6,5 +6,5 @@ type Item struct {
 	Note   string // note on the item itself
 	Amount int    // how much of an item should be bought
 	Status string // status of item, can be [new old deleted] can be expanded throughout development
-	Cat_id int
+	Cat_id int    // foreign key, id of category, snake_case to keep consistent with database schema
 }
